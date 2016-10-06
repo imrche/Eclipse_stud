598d4dbc74484272a03755b5dd6972db80ce0c4e
imrche <richy.corp@gmail.com>
2016-10-06 23:48:02 +0600

    обновление

---
+++ src/Form/ovalpkg.java
@@ -0,0 +1,13 @@
+package Form;
+
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+public class ovalpkg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
